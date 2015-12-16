a9bca5d253a938c982a191393955900749762c46
yakho213 <yakho213@gmail.com>
2015-12-16 03:58:22 +0000

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  //"your_seed_ip1.com:8080",
-  //"your_seed_ip2.com:8080",
+  "yakho213.ddns.net:17213",
+  "yakho213.ddns.net:18213",
 };
 
 struct CheckpointData {
